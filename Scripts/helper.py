fc71e19e160d7019f95cc7eb1ea81288a0b3cc5b
jgonzet <ignacio.gonzet@gmail.com>
2024-06-22 19:41:27 -0600

    Se termino de hacer funcionar a Pelicula.get_stats()

--- Scripts/helper.py
+++ Scripts/helper.py
@@ -1,6 +1,9 @@
 cols_df_movies = ["id", "Name", "Release Date", "IMDB URL", "unknown", "Action","Adventure","Animation", "Children's", "Comedy", "Crime","Documentary", "Drama", "Fantasy", "Film-Noir","Horror", "Musical","Mystery", "Romance","Sci-Fi", "Thriller", "War", "Western"]
 pos_gens = 4
 
+def get_gens():
+    return cols_df_movies[pos_gens:]
+
 def parse_movie(pelicula, df_movies):
     new_row = [pelicula.id,pelicula.nombre,"01-Jan-"+str(pelicula.anio),"unkown URL"]
     if len(pelicula.generos) == 0: new_row.append(1)
@@ -33,44 +36,4 @@ def gen_from_one_hot(serie):
     serie_aux = serie[cols_df_movies[pos_gens:]]
     return serie_aux.index[serie_aux==1].tolist()
 
-    @classmethod
-    def get_stats(cls, df_mov, anios=None, generos=None):
-        # Este class method imprime una serie de estadísticas calculadas sobre
-        # los resultados de una consulta al DataFrame df_mov. 
-        # Las estadísticas se realizarán sobre las filas que cumplan con los requisitos de:
-        # anios: [desde_año, hasta_año]
-        # generos: [generos]
-        # Las estadísticas son:
-        # - Datos película más vieja
-        # - Datos película más nueva
-        # - Bar plots con la cantidad de películas por año/género.
-        # if anios:
-        #     desde_año, hasta_año = anios
-        #     df_mov = df_mov[(df_mov['año'] >= desde_año) & (df_mov['año'] <= hasta_año)]
-        # if generos:
-        #     df_mov = df_mov[df_mov['género'].isin(generos)]
-        anitos, generitos = anios, generos
-        movies_selected = cls.get_from_df(df_mov, anios = anitos, generos = generitos)
-        print(movies_selected)
-
-        # pelicula_mas_vieja = df_mov.loc[df_mov['año'].idxmin()]
-        # print("Película más vieja:")
-        # print(pelicula_mas_vieja)
-        
-        # pelicula_mas_nueva = df_mov.loc[df_mov['año'].idxmax()]
-        # print("\nPelícula más nueva:")
-        # print(pelicula_mas_nueva)
-        
-        # plt.figure(figsize=(10, 6))
-        # df_mov['año'].value_counts().sort_index().plot(kind='bar')
-        # plt.title('Cantidad de películas por año')
-        # plt.xlabel('Año')
-        # plt.ylabel('Cantidad de películas')
-        # plt.show()
-        
-        # plt.figure(figsize=(10, 6))
-        # df_mov['género'].value_counts().plot(kind='bar')
-        # plt.title('Cantidad de películas por género')
-        # plt.xlabel('Género')
-        # plt.ylabel('Cantidad de películas')
-        # plt.show()
+    
